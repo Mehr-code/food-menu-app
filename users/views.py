33b0c027297c4fe13a3d8ce7fbcdf0e59e7ae6ec
Mehr-code <papa.molla8@gmail.com>
2025-11-28 17:32:03 +0330

    feat(auth): add register feature with flash messages

--- users/views.py
+++ users/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 
 def register(req):
+    if req.method == "POST":
+        form = UserCreationForm(req.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(
+                req, f"خوش آمدید {username} ! حساب کاربری شما با موفقیت ایجاد شد."
+            )
+            return redirect("menu:index")
+
     form = UserCreationForm()
     context = {"form": form}
     return render(req, "users/register.html", context)
